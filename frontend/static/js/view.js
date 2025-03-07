10ac9760b3f5f150a63ba6b59435b79dfa17f727
Lucky032 <laxmankarnam04@gmail.com>
2025-03-07 11:45:34 +0530

    Initial commit - Added Indian Sports Hub project

---
+++ frontend/static/js/view.js
@@ -0,0 +1,9 @@
+document.getElementById('Next').addEventListener('click', function() {
+    // Call a function to fetch the next profile data
+    fetchNextProfile();
+});
+
+document.getElementById('Previous').addEventListener('click', function() {
+    // Call a function to fetch the previous profile data
+    fetchPrevProfile();
+});
